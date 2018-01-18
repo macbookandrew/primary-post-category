84be113d8b16e6589554207030ad0156d3704766
Andrew Minion <andrew@andrewrminion.com>
2018-01-17 19:35:11 -0500

    starter plugin

---
+++ primary-post-category.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Primary Post Category
+ * Plugin URI: https://github.com/macbookandrew/primary-post-category
+ * Description: A simple plugin to assign a primary and multiple secondary categories to posts
+ * Version: 1.0
+ * Author: AndrewRMinion Design
+ * Author URI: https://andrewrminion.com/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
